1fe83ca92c4687aeb85b01b8b504def1a51a5a55
Jary <zhenguoyou@neucloud.cn>
2017-12-19 18:26:50 +0800

    add kstream

--- kafka/src/main/java/note/kafka/LocalKafka.java
+++ kafka/src/main/java/note/kafka/LocalKafka.java
@@ -1,4 +1,4 @@
-package note.kafka.v11;
+package note.kafka;
 
 import kafka.server.KafkaConfig;
 import kafka.server.KafkaServerStartable;
@@ -37,12 +37,13 @@ public  class LocalKafka {
             kafkaProperties.setProperty("broker.id", String.valueOf(KafkaProperties.BROKER_ID));
             kafkaProperties.setProperty("zookeeper.connect", KafkaProperties.ZOOKEEPER_CONNECT);
             kafkaProperties.setProperty("log.dirs", kafkaTmpLogsDir.getAbsolutePath());
+            //kafkaProperties.setProperty("num.partitions", "2");
 
             KafkaConfig kafkaConfig = new KafkaConfig(kafkaProperties);
+
             KafkaServerStartable kafka = new KafkaServerStartable(kafkaConfig);
-            //start
             kafka.startup();
-            System.out.println("started kafka ok");
+            System.out.println("start kafka ok "+kafka.serverConfig().numPartitions());
         }
     }
 
@@ -86,37 +87,6 @@ public  class LocalKafka {
             } else {
                 System.out.println("Failed to delete or create domain dir for Zookeeper");
             }
-        }
-
-
-        public void startZK() throws Exception {
-            ZooKeeper zk = new ZooKeeper("localhost:2222", 10000,
-                    new Watcher() {
-                        public void process(WatchedEvent event) {
-                            System.out.println("event: " + event.getType());
-                        }
-                    });
-
-            System.out.println(zk.getState());
-
-            zk.create("/myApps", "myAppsData".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-            zk.create("/myApps/App1", "App1Data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-            zk.create("/myApps/App2", "App2Data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-            zk.create("/myApps/App3", "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-            zk.setData("/myApps/App3", "App3Data".getBytes(), -1);
-
-            System.out.println(zk.exists("/myApps", true));
-            System.out.println(new String(zk.getData("/myApps", true, null)));
-
-            List<String> children = zk.getChildren("/myApps", true);
-            for (String child : children) {
-                System.out.println(new String(zk.getData("/myApps/" + child, true, null)));
-                zk.delete("/myApps/" + child, -1);
-            }
-
-            //zk.delete("/myApps",-1);
-
-            zk.close();
         }
     }
 }
--- kafka/src/main/java/note/zookeeper/ZkConnect.java
+++ kafka/src/main/java/note/zookeeper/ZkConnect.java
@@ -1,9 +1,10 @@
 package note.zookeeper;
 
 
-import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.*;
 import org.apache.zookeeper.ZooDefs.Ids;
-import org.apache.zookeeper.ZooKeeper;
+
+import java.util.List;
 
 public class ZkConnect {
     public static void main (String args[]) throws Exception
@@ -34,4 +35,33 @@ public class ZkConnect {
     }
 
 
+    public void startZK() throws Exception {
+        ZooKeeper zk = new ZooKeeper("localhost:2222", 10000,
+                new Watcher() {
+                    public void process(WatchedEvent event) {
+                        System.out.println("event: " + event.getType());
+                    }
+                });
+
+        System.out.println(zk.getState());
+
+        zk.create("/myApps", "myAppsData".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        zk.create("/myApps/App1", "App1Data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        zk.create("/myApps/App2", "App2Data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        zk.create("/myApps/App3", "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        zk.setData("/myApps/App3", "App3Data".getBytes(), -1);
+
+        System.out.println(zk.exists("/myApps", true));
+        System.out.println(new String(zk.getData("/myApps", true, null)));
+
+        List<String> children = zk.getChildren("/myApps", true);
+        for (String child : children) {
+            System.out.println(new String(zk.getData("/myApps/" + child, true, null)));
+            zk.delete("/myApps/" + child, -1);
+        }
+
+        //zk.delete("/myApps",-1);
+
+        zk.close();
+    }
 }
