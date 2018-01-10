a16341ec6658bf57ace5a24ac95b9a050077f31d
Jary <zhenguoyou@neucloud.cn>
2018-01-10 17:50:12 +0800

    some test

--- hazelcast/src/main/java/note/hc/zk/Client.java
+++ hazelcast/src/main/java/note/hc/zk/Client.java
@@ -3,11 +3,21 @@ package note.hc.zk;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
 import com.hazelcast.config.DiscoveryStrategyConfig;
+import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.core.Member;
 import com.hazelcast.instance.GroupProperty;
 import com.hazelcast.zookeeper.ZookeeperDiscoveryProperties;
 import com.hazelcast.zookeeper.ZookeeperDiscoveryStrategyFactory;
+import org.apache.curator.test.TestingServer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Jary on 2018/1/10 0010.
@@ -15,19 +25,36 @@ import com.hazelcast.zookeeper.ZookeeperDiscoveryStrategyFactory;
 public class Client {
 
     public static void main(String[] args) {
-        String zookeeperURL = "localhost:2181";
+        String zookeeperURL = "kafka1:2222";
+        final int HAZELCAST_BASE_PORT = 9999;
 
         ClientConfig config = new ClientConfig();
+        //config.getNetworkConfig().getAwsConfig().setEnabled(false);
         config.setProperty(GroupProperty.DISCOVERY_SPI_ENABLED, "true");
-        config.getNetworkConfig().getAwsConfig().setEnabled(false);
+        List add = new ArrayList();
+        add.add("localhost:"+HAZELCAST_BASE_PORT);
+
+        config.getNetworkConfig().setAddresses(add);
 
         DiscoveryStrategyConfig discoveryStrategyConfig = new DiscoveryStrategyConfig(new ZookeeperDiscoveryStrategyFactory());
         discoveryStrategyConfig.addProperty(ZookeeperDiscoveryProperties.ZOOKEEPER_URL.key(),zookeeperURL);
-        discoveryStrategyConfig.addProperty(ZookeeperDiscoveryProperties.ZOOKEEPER_PATH.key(), "/hazelcast");
-        discoveryStrategyConfig.addProperty(ZookeeperDiscoveryProperties.GROUP.key(), "{clusterId}");
+        discoveryStrategyConfig.addProperty(ZookeeperDiscoveryProperties.ZOOKEEPER_PATH.key(), "/hazelcast2");
+        discoveryStrategyConfig.addProperty(ZookeeperDiscoveryProperties.GROUP.key(), "id");
         config.getNetworkConfig().getDiscoveryConfig().addDiscoveryStrategyConfig(discoveryStrategyConfig);
 
-        HazelcastInstance instance  = HazelcastClient.newHazelcastClient(config);
+
+            HazelcastInstance instance  = HazelcastClient.newHazelcastClient(config);
+        IMap map = instance.getMap("customers");
+        System.out.println("Map Size:" + map.size());
+
+            Iterator<Member> ite  = instance.getCluster().getMembers().iterator();
+            while (ite.hasNext()){
+
+                Member m = ite.next();
+                System.out.println(m.getAddress()+"  "+m.getSocketAddress().toString());
+
+            }
 
     }
+
 }
--- hazelcast/src/main/java/note/hc/zk/Server.java
+++ hazelcast/src/main/java/note/hc/zk/Server.java
@@ -3,9 +3,12 @@ package note.hc.zk;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.DiscoveryStrategyConfig;
 import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.instance.GroupProperty;
 import com.hazelcast.zookeeper.ZookeeperDiscoveryProperties;
 import com.hazelcast.zookeeper.ZookeeperDiscoveryStrategyFactory;
+
+import java.util.Map;
 
 /**
  * Created by Jary on 2018/1/10 0010.
@@ -13,19 +16,25 @@ import com.hazelcast.zookeeper.ZookeeperDiscoveryStrategyFactory;
 public class Server {
     public static void main(String[] args) {
 
-        String zookeeperURL = "localhost:2181";
+        String zookeeperURL = "kafka1:2222";
+        final int HAZELCAST_BASE_PORT = 9999;
 
         Config config = new Config();
         config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
         config.setProperty(GroupProperty.DISCOVERY_SPI_ENABLED, "true");
+        config.getNetworkConfig().setPort(HAZELCAST_BASE_PORT);
 
         DiscoveryStrategyConfig discoveryStrategyConfig = new DiscoveryStrategyConfig(new ZookeeperDiscoveryStrategyFactory());
         discoveryStrategyConfig.addProperty(ZookeeperDiscoveryProperties.ZOOKEEPER_URL.key(), zookeeperURL);
-        discoveryStrategyConfig.addProperty(ZookeeperDiscoveryProperties.ZOOKEEPER_PATH.key(), "/hazelcast");
-        discoveryStrategyConfig.addProperty(ZookeeperDiscoveryProperties.GROUP.key(), "{clusterId}");
+        discoveryStrategyConfig.addProperty(ZookeeperDiscoveryProperties.ZOOKEEPER_PATH.key(), "/hazelcast2");
+        discoveryStrategyConfig.addProperty(ZookeeperDiscoveryProperties.GROUP.key(), "id");
 
         config.getNetworkConfig().getJoin().getDiscoveryConfig().addDiscoveryStrategyConfig(discoveryStrategyConfig);
 
-        Hazelcast.newHazelcastInstance(config);
+        HazelcastInstance instance =  Hazelcast.newHazelcastInstance(config);
+        Map<Integer, String> mapCustomers = instance.getMap("customers");
+        mapCustomers.put(1, "Joe");
+        mapCustomers.put(2, "Ali");
+        mapCustomers.put(3, "Avi");
     }
 }
