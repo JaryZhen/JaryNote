35886edf9c5b1f483fbfbfa602e9de23bc2f0387
Jary <jary@JarydeMacBook-Pro.local>
2021-07-28 15:18:01 +0800

    add del learn func

--- algorithm/src/main/java/stack_queue_list/ListTest.java
+++ algorithm/src/main/java/stack_queue_list/ListTest.java
@@ -26,21 +26,84 @@ public class ListTest {
     }
 
     /**
-     * @param args
+     * 将单链表按基数分 左边小 中间 右边大
+     * 1. 放数组里，在数组上partion
+     * 2. 分三部分 再链接
      */
 
+    public ListNode split(ListNode<Integer> head, int val) {
+        ListNode<Integer> lessHead = null;
+        ListNode<Integer> lessTail = null;
+        ListNode<Integer> midHead = null;
+        ListNode<Integer> midTail = null;
+        ListNode<Integer> moreHead = null;
+        ListNode<Integer> moreTail = null;
+
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = null;
+            if (head.data < val) {
+                if (lessHead == null) {
+                    lessHead = head;
+                    lessTail = head;
+                } else {
+                    lessTail.next = head;
+                    lessTail = head;
+                }
+            } else if (head.data == val) {
+                if (midHead == null) {
+                    midHead = head;
+                    midTail = head;
+                } else {
+                    midHead.next = head;
+                    midTail = head;
+                }
+            } else {
+                if (moreHead == null) {
+                    moreHead = head;
+                    moreTail = head;
+                } else {
+                    moreTail.next = head;
+                    moreTail = head;
+                }
+            }
+            head = next;
+        }
+        if (lessHead != null) {
+            if (midHead != null) {
+                lessTail.next = midHead;
+                midTail.next = moreHead;
+                return lessHead;
+            } else {
+                lessTail.next = moreHead;
+                return lessHead;
+            }
+        } else {
+            if (midHead != null) {
+                midTail.next = moreHead;
+                return midHead;
+            }
+        }
+        return moreHead;
+    }
+
+
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         ListTest test = new ListTest();
         //测试多少次
-        for (int j = 0; j < 20; j++) {
+        for (int j = 0; j < 1; j++) {
             // 构造数据
-            ListNode listNode = new ListNode("1", null, null);
+            ListNode<Integer> listNode = new ListNode(1, null, null);
             ListNode head = listNode;
             ListNode tail = listNode;
             int lage = new Random().nextInt(15);
-            System.out.print("size: " + (lage + 1 )+ ": ");
+            System.out.print("size: " + (lage + 1) + ": ");
             for (int i = 0; i < lage; i++) {
-                ListNode temp = new ListNode(new Random().nextInt(1000) + "", null, null);
+                ListNode temp = new ListNode(new Random().nextInt(15), null, null);
                 tail.next = temp;
                 tail = temp;
             }
@@ -54,7 +117,13 @@ public class ListTest {
             /**
              * hers test
              */
-            System.out.println("中点： " + test.midNode(head));
+            //System.out.println("中点： " + test.midNode(head));
+
+            ListNode re = test.split(head, 1);
+            while (re != null) {
+                System.out.println("re: " + re.data);
+                re = re.next;
+            }
         }
     }
 }
