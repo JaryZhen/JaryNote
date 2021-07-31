0f7be47dab41a7c256b39e0c964297aca970af24
jaryzhen <951194829@qq.com>
2021-07-31 17:49:00 +0800

    add 并查集

--- exe.sh
+++ exe.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 
-git remote set-url origin https://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github.com/JaryZhen/JaryNote.git
+git remote set-url origin https://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@github.com/JaryZhen/JaryNote.git
 
 git add .
 
