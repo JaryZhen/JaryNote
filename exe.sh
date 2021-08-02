190e976781b6124e52ac07fd37b86773a9a0fe0e
jaryzhen <951194829@qq.com>
2021-08-02 16:04:58 +0800

    add

--- exe.sh
+++ exe.sh
@@ -1,14 +1,13 @@
 #!/bin/zsh
-
-
-git remote set-url origin https://@github.com/JaryZhen/JaryNote.git
+##git remote set-url origin https://@github.com/JaryZhen/JaryNote.git
 
 git add .
 
 git status
 
-git commit -m "add 并查集"
+git commit -m "add "
 
 git status
+
 
 git push
