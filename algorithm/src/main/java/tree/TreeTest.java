fe687b6e6cd783279ab40124ae196ef3ab924969
jaryzhen <951194829@qq.com>
2021-08-23 19:56:22 +0800

    矩阵最大面积

--- algorithm/src/main/java/tree/TreeTest.java
+++ algorithm/src/main/java/tree/TreeTest.java
@@ -319,11 +319,39 @@ public class TreeTest {
         return is;
     }
 
+    private Map<Integer, Integer> indexMap;
+
+    public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) {
+            return null;
+        }
+        int preorder_root = preorder_left;
+        int inorder_root = indexMap.get(preorder[preorder_root]);
+        TreeNode root = new TreeNode(preorder[preorder_root]);
+        int size_left_subtree = inorder_root - inorder_left;
+        root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        // 构造哈希映射，帮助我们快速定位根节点
+        indexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+
+
     public static void main(String[] args) {
         TreeTest test = new TreeTest();
 
-        String[] s = new String[]{"2", null};
-        TreeNode head = test.createTreeNode(s);
+        int[] a = new int[]{8, 4, 3, 5, 12, 6, 88, 99, 13};
+        int[] b = new int[]{3, 4, 5, 8, 6, 88, 99, 12, 13};
+        TreeNode head = test.buildTree(a, b);
         /*
               8
             /   \
@@ -356,29 +384,5 @@ public class TreeTest {
         System.out.println(test.isSymmetric(head));
     }
 
-    private Map<Integer, Integer> indexMap;
-
-    public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
-        if (preorder_left > preorder_right) {
-            return null;
-        }
-        int preorder_root = preorder_left;
-        int inorder_root = indexMap.get(preorder[preorder_root]);
-        TreeNode root = new TreeNode(preorder[preorder_root]);
-        int size_left_subtree = inorder_root - inorder_left;
-        root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
-        root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
-        return root;
-    }
-
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n = preorder.length;
-        // 构造哈希映射，帮助我们快速定位根节点
-        indexMap = new HashMap<Integer, Integer>();
-        for (int i = 0; i < n; i++) {
-            indexMap.put(inorder[i], i);
-        }
-        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
-    }
 
 }
