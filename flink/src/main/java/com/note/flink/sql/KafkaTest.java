7b9a6ecd7c2523463134882f6c2613951b828006
jaryzhen <951194829@qq.com>
2021-10-20 15:07:26 +0800

    add ka wm

--- flink/src/main/java/com/note/flink/sql/KafkaTest.java
+++ flink/src/main/java/com/note/flink/sql/KafkaTest.java
@@ -29,7 +29,7 @@ public class KafkaTest {
             + "  `headers` MAP<STRING, BYTES> METADATA,"
             + "  `partition` INT METADATA VIRTUAL,"
             + "  `topic` STRING METADATA VIRTUAL,"
-            + "     `offset` BIGINT METADATA VIRTUAL,"
+            + "   `offset` BIGINT METADATA VIRTUAL,"
             + "  role STRING,"
             + "  user_type STRING,"
             + "  local_ping INT,"
@@ -37,7 +37,8 @@ public class KafkaTest {
             + "  server_rtt INT,"
             + "  classroom STRING,"
             + "  full_ping STRING,"
-            + " `timestamp` BIGINT"
+            + " `timestamp` BIGINT,"
+            + " WATERMARK FOR timestamp AS timestamp - INTERVAL '1' SECOND"
             + ") WITH (" +
             "  'connector' = 'kafka'," +
             "  'topic' = 'app_inline_room_anticipation_ft_sql_test'," +
@@ -56,13 +57,15 @@ public class KafkaTest {
             + "  server_rtt INT,"
             + "  classroom STRING,"
             + "  full_ping STRING,"
-            + "  `timestamp` BIGINT"
+            + "  `timestamp` BIGINT,"
+            + "  PRIMARY KEY (classroom) NOT ENFORCED"
             + ") WITH (" +
-            "  'connector' = 'kafka'," +
+            "  'connector' = 'upsert-kafka'," +
             "  'topic' = 'app_inline_room_anticipation_ft_sql_test_sink'," +
-            "  'properties.bootstrap.servers' = 'localhost:9092'," +
-            "  'format' = 'json'" +
-            ")";
+            "  'properties.bootstrap.servers' = 'localhost:9092',"
+            + " 'key.format' = 'avro',"
+            + " 'value.format' = 'avro'"
+            + ")";
 
     static String print =
             "CREATE TABLE kafkaSink ( role STRING, " +
@@ -94,78 +97,8 @@ public class KafkaTest {
 
     public static void test1() throws Exception {
         tEnv.executeSql(ks);
-
-/*     String sql = "SELECT * FROM kafkaSource";
-        Table table = tEnv.sqlQuery(sql);
-        table.printSchema();
-        DataStream<Row> resultDs = tEnv.toChangelogStream(table);
-        resultDs.print();
-        */
         tEnv.executeSql(ksink);
-        tEnv.executeSql(insert);  //从结果表查数据，转存到输出表
-        // tEnv.executeSql("insert into output_kafka select * from "+tableResult);  //从结果表查数据，转存到输出表
-
+        tEnv.executeSql(insert);
         //env.execute();
-    }
-
-    public static void test() throws Exception {
-       /* final String createTable =
-                String.format(
-                        "CREATE TABLE kafka (\n"
-                                + "  `physical_1` STRING,\n"
-                                + "  `physical_2` INT,\n"
-                                // metadata fields are out of order on purpose
-                                // offset is ignored because it might not be deterministic
-                                + "  `timestamp-type` STRING METADATA VIRTUAL,\n"
-                                + "  `timestamp` TIMESTAMP(3) METADATA,\n"
-                                + "  `leader-epoch` INT METADATA VIRTUAL,\n"
-                                + "  `headers` MAP<STRING, BYTES> METADATA,\n"
-                                + "  `partition` INT METADATA VIRTUAL,\n"
-                                + "  `topic` STRING METADATA VIRTUAL,\n"
-                                + "  `physical_3` BOOLEAN\n"
-                                + ") WITH (\n"
-                                + "  'connector' = 'kafka',\n"
-                                + "  'topic' = '%s',\n"
-                                + "  'properties.bootstrap.servers' = '%s',\n"
-                                + "  'properties.group.id' = '%s',\n"
-                                + "  'scan.startup.mode' = 'earliest-offset',\n"
-                                + "  %s\n"
-                                + ")");
-*/
-        tEnv.executeSql(ks);
-
-        String initialValues =
-                "INSERT INTO kafka\n"
-                        + "VALUES\n"
-                        + " ('data 1', 1, TIMESTAMP '2020-03-08 13:12:11.123', MAP['k1', X'C0FFEE', 'k2', X'BABE'], TRUE),\n"
-                        + " ('data 2', 2, TIMESTAMP '2020-03-09 13:12:11.123', CAST(NULL AS MAP<STRING, BYTES>), FALSE),\n"
-                        + " ('data 3', 3, TIMESTAMP '2020-03-10 13:12:11.123', MAP['k1', X'10', 'k2', X'20'], TRUE)";
-        //tEnv.executeSql(initialValues).await();
-
-
-        final List<Row> result = collectRows(tEnv.sqlQuery("SELECT * FROM kafkaSource"), 3);
-
-        System.out.println(result.toArray());
-    }
-
-    public static List<Row> collectRows(Table table, int expectedSize) throws Exception {
-        final TableResult result = table.execute();
-        final List<Row> collectedRows = new ArrayList<>();
-        try (CloseableIterator<Row> iterator = result.collect()) {
-            while (collectedRows.size() < expectedSize && iterator.hasNext()) {
-                collectedRows.add(iterator.next());
-            }
-        }
-        result.getJobClient()
-                .ifPresent(
-                        jc -> {
-                            try {
-                                jc.cancel().get(5, TimeUnit.SECONDS);
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
-                        });
-
-        return collectedRows;
     }
 }
--- flink/src/main/java/com/note/flink/sql/KafkaTest2.java
+++ flink/src/main/java/com/note/flink/sql/KafkaTest2.java
@@ -38,42 +38,11 @@ public class KafkaTest2 {
             "  'connector' = 'kafka'," +
             "  'topic' = 'app_inline_room_anticipation_ft_sql_test_sink'," +
             "  'properties.bootstrap.servers' = 'localhost:9092'," +
-            "  'properties.group.id' = 'testGroup'," +
-            "  'scan.startup.mode' = 'earliest-offset'," +
-            "  'format' = 'json'"
+            "  'properties.group.id' = 'testGroupSink'," +
+            "  'scan.startup.mode' = 'latest-offset'," +
+            "  'format' = 'avro'"
             + ")";
 
-    static String ksink = "CREATE TABLE kafkaSink ("
-            // + " `offset` BIGINT,"
-            + "  role STRING,"
-            + "  user_type STRING,"
-            + "  local_ping INT,"
-            + "  line INT,"
-            + "  server_rtt INT,"
-            + "  classroom STRING,"
-            + "  full_ping STRING,"
-            + "  `timestamp` BIGINT"
-            + ") WITH (" +
-            "  'connector' = 'kafka'," +
-            "  'topic' = 'app_inline_room_anticipation_ft_sql_test_sink'," +
-            "  'properties.bootstrap.servers' = 'localhost:9092'" +
-            ")";
-
-    static String print =
-            "CREATE TABLE kafkaSink ( role STRING, " +
-                    " user_type STRING, " +
-                    " local_ping INT, " +
-                    " line INT,  server_rtt INT, " +
-                    " classroom STRING, " +
-                    " full_ping STRING, " +
-                    " `timestamp` BIGINT" +
-                    ") WITH ( " +
-                    " 'connector' = 'print'" +
-                    ")";
-
-    static String insert = " insert into kafkaSink " +
-            "select role, user_type, local_ping, line, server_rtt, classroom, full_ping, `timestamp` " +
-            "from kafkaSource";
 
     static StreamExecutionEnvironment env;
     static StreamTableEnvironment tEnv;
@@ -82,8 +51,6 @@ public class KafkaTest2 {
         env = StreamExecutionEnvironment.getExecutionEnvironment();
         tEnv = StreamTableEnvironment.create(env);
         System.out.println(ks);
-        System.out.println(ksink);
-        System.out.println(insert);
         test1();
     }
 
@@ -95,52 +62,9 @@ public class KafkaTest2 {
         table.printSchema();
         DataStream<Row> resultDs = tEnv.toChangelogStream(table);
         resultDs.print();
-        //tEnv.executeSql(ksink);
-        //tEnv.executeSql(insert);  //从结果表查数据，转存到输出表
-        // tEnv.executeSql("insert into output_kafka select * from "+tableResult);  //从结果表查数据，转存到输出表
-
         env.execute();
     }
 
-    public static void test() throws Exception {
-       /* final String createTable =
-                String.format(
-                        "CREATE TABLE kafka (\n"
-                                + "  `physical_1` STRING,\n"
-                                + "  `physical_2` INT,\n"
-                                // metadata fields are out of order on purpose
-                                // offset is ignored because it might not be deterministic
-                                + "  `timestamp-type` STRING METADATA VIRTUAL,\n"
-                                + "  `timestamp` TIMESTAMP(3) METADATA,\n"
-                                + "  `leader-epoch` INT METADATA VIRTUAL,\n"
-                                + "  `headers` MAP<STRING, BYTES> METADATA,\n"
-                                + "  `partition` INT METADATA VIRTUAL,\n"
-                                + "  `topic` STRING METADATA VIRTUAL,\n"
-                                + "  `physical_3` BOOLEAN\n"
-                                + ") WITH (\n"
-                                + "  'connector' = 'kafka',\n"
-                                + "  'topic' = '%s',\n"
-                                + "  'properties.bootstrap.servers' = '%s',\n"
-                                + "  'properties.group.id' = '%s',\n"
-                                + "  'scan.startup.mode' = 'earliest-offset',\n"
-                                + "  %s\n"
-                                + ")");
-*/
-        tEnv.executeSql(ks);
-
-        String initialValues =
-                "INSERT INTO kafka\n"
-                        + "VALUES\n"
-                        + " ('data 1', 1, TIMESTAMP '2020-03-08 13:12:11.123', MAP['k1', X'C0FFEE', 'k2', X'BABE'], TRUE),\n"
-                        + " ('data 2', 2, TIMESTAMP '2020-03-09 13:12:11.123', CAST(NULL AS MAP<STRING, BYTES>), FALSE),\n"
-                        + " ('data 3', 3, TIMESTAMP '2020-03-10 13:12:11.123', MAP['k1', X'10', 'k2', X'20'], TRUE)";
-        //tEnv.executeSql(initialValues).await();
-
-
-        final List<Row> result = collectRows(tEnv.sqlQuery("SELECT * FROM kafkaSource"), 3);
-
-        System.out.println(result.toArray());
-    }
 
     public static List<Row> collectRows(Table table, int expectedSize) throws Exception {
         final TableResult result = table.execute();
