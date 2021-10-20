f1e9e1e31cdd82f2afabefa20469f778f288f2a5
jaryzhen <951194829@qq.com>
2021-10-20 17:51:18 +0800

    add ka wm

--- flink/src/main/java/com/note/flink/sql/KafkaTest.java
+++ flink/src/main/java/com/note/flink/sql/KafkaTest.java
@@ -2,6 +2,10 @@ package com.note.flink.sql;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.module.ModuleManager;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.assertThat;
 
@@ -11,6 +15,8 @@ import static org.junit.Assert.assertThat;
  * @Date: 2021/10/18 2:39 下午
  */
 public class KafkaTest {
+    private static final Logger LOG = LoggerFactory.getLogger(ModuleManager.class);
+
     static String source_kafka = "CREATE TABLE kafkaSource ("
             + "  `timestamp-type` STRING METADATA VIRTUAL,"
             + "  `timestamp` TIMESTAMP(3) METADATA,"
@@ -83,7 +89,7 @@ public class KafkaTest {
             "select role, user_type, local_ping, line, server_rtt, classroom, full_ping, ts " +
             "from kafkaSource ";
 
-    static String insertPrim =
+    static String insert_kafka_Prim =
             " insert into kafkaSink " +
             "select classroom, count(*) as total " +
             "from kafkaSource"
@@ -95,6 +101,7 @@ public class KafkaTest {
     public static void main(String[] args) throws Exception {
         env = StreamExecutionEnvironment.getExecutionEnvironment();
         tEnv = StreamTableEnvironment.create(env);
+        LOG.info("asdfasdfa");
         System.out.println(source_kafka);
         System.out.println(sink_print);
         System.out.println(insert);
@@ -103,8 +110,9 @@ public class KafkaTest {
 
     public static void test1() throws Exception {
         tEnv.executeSql(source_kafka);
-        tEnv.executeSql(sink_print);
-        tEnv.executeSql(insert);
+        tEnv.executeSql(sink_kafka_Prim);
+        tEnv.executeSql(insert_kafka_Prim);
         //env.execute();
+
     }
 }
