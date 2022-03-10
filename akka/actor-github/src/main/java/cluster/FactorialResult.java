84a949fdd56eaf74f2ee98f2cb8329750003e190
jaryzhen <jaryzhen@tencent.com>
2022-03-10 11:26:03 +0800

    add akka cluster

---
+++ akka/actor-github/src/main/java/cluster/FactorialResult.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2018-2022 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package cluster;
+
+import java.math.BigInteger;
+import java.io.Serializable;
+
+public class FactorialResult implements Serializable {
+  private static final long serialVersionUID = 1L;
+  public final int n;
+  public final BigInteger factorial;
+
+  FactorialResult(int n, BigInteger factorial) {
+    this.n = n;
+    this.factorial = factorial;
+  }
+}
